0411ffe227622a086e3c037d8f6c7d261d38da9c
Lazy-Shadow <thederpsmc3@gmail.com>
2026-03-22 18:22:17 +0800

    localhost

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
